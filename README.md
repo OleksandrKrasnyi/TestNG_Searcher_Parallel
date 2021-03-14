38bb10cc6a55e959e9987b17ac6b80a4a6d74666
OleksandrKrasnyi <oskrasnyi@gmail.com>
2021-03-14 20:38:27 +0200

    Add project v.0.0.3

---
+++ README.md
@@ -0,0 +1,14 @@
+## TestNG Searcher Parallel
+
+This maven project contains  
+3 tests which can be run **separately** or **in parallel**.
+
+ 
+To start test **separately** run one of files below:   
+[SearcherParallel1](src/test/java/SearcherParallel1.java)  
+[SearcherParallel2](src/test/java/SearcherParallel1.java)  
+[SearcherParallel3](src/test/java/SearcherParallel1.java)
+
+To run all 3 test **in parallel** run file:  
+[testng.xml](testng.xml)  
+
---
+++ src/test/java/SearcherParallel1.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class SearcherParallel1 {
+
+    private WebDriver driver;
+    public static final String SEARCH_FIELD = "/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input";
+    public static final String SEARCH_ITEM = "iPhone 12";
+    public static final String SEARCH_BUTTON = "/html/body/div[1]/div[3]/form/div[2]/div[1]/div[3]/center/input[1]";
+
+    @BeforeTest
+    public void preCondition() {
+        // set up source for search activities
+        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
+        driver = new ChromeDriver();
+        String urlHomePage = "https://www.google.com/";
+        driver.get(urlHomePage);
+    }
+
+    @Test
+    public void searchItem() {
+        // go to google.com / take
+        driver.findElement(By.xpath(SEARCH_FIELD)).sendKeys(SEARCH_ITEM);
+        driver.findElement(By.xpath(SEARCH_BUTTON)).click();
+    }
+
+    @AfterTest
+    public void afterTest() throws InterruptedException {
+        Thread.sleep(5000);
+        driver.close();
+    }
+}
